add877e184f7f1c6daefb60318ef5e2c7a1e8221
Vasilis Moulas <vasilismoulas11@gmail.com>
2024-11-12 13:10:31 +0200

    update: Singleton dp. fixed

--- tic-tac-toe-api/src/frontend/ui/symbol_table.py
+++ tic-tac-toe-api/src/frontend/ui/symbol_table.py
@@ -25,7 +25,7 @@ class SingletonMeta(type):
         the returned instance.
         """
         if cls not in cls._instances:
-            instance = super().__call__(*args, **kwargs)
+            instance = super().__call__(*args, **kwargs) #it invokes "__init__" function/constructor bydefault
             cls._instances[cls] = instance
         return cls._instances[cls]
 
@@ -36,8 +36,17 @@ class Symbol_Table(ISymbol_Table, metaclass=SingletonMeta):
     """
 
     def __init__(self) -> None:
-      self.__hash_map = {}  
-
+        self.__hash_map = {}  
+    
+    @classmethod
+    def _get_instance(cls):
+        """
+        Internal method to get or create the singleton instance.
+        """
+        if cls not in SingletonMeta._instances:
+            cls._instances[cls] = super(SingletonMeta, cls).__call__()
+        return SingletonMeta._instances[cls]
+    
     @property
     def hash_map(self, value: tuple[str, Tsymbol]) -> None:
         if((type(value[0])==str) and (type(value[1])==Tsymbol)):
